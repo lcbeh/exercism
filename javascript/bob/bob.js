bb06f0ad8348f3228d4a488a279d78193ab9d087
Li Chien <lichien2010@yahoo.co.uk>
2017-01-10 11:42:39 +0000

    Bob

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,10 +1,27 @@
 var Bob = function() {};
 
+var silent = function(input) {
+  return input.trim() === "";
+};
+
+var shouted = function(input) {
+  return input.toUpperCase() === input && input.toUpperCase() !== input.toLowerCase();
+};
+
+var question = function(input) {
+  return input[input.length - 1] === "?";
+};
+
 Bob.prototype.hey = function(input) {
-  if (input === false) return 'Fine. Be that way!';
-  if (input === input.toUpperCase()) return 'Whoa, chill out!';
-  if (input.includes('?')) return 'Sure.';
-  return 'Whatever.';
+  if (silent(input)) {
+    return "Fine. Be that way!";
+  } else if (shouted(input)) {
+    return "Whoa, chill out!";
+  } else if (question(input)) {
+    return "Sure.";
+  } else {
+    return 'Whatever.';
+  }
 };
 
 module.exports = Bob;
